dcc9d23a33bdbe126716896f413860245071b298
Dobbelina2 <132213227+Dobbelina2@users.noreply.github.com>
2023-05-19 10:19:05 +0200

    remove rename

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,12 +24,7 @@ git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
 git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@$INPUT_GIT_SERVER/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
-if [ ! -z "$INPUT_RENAME" ]; then
-  echo "Setting new filename: ${INPUT_RENAME}"
-  DEST_COPY="$CLONE_DIR/$INPUT_DESTINATION_FOLDER/$INPUT_RENAME"
-else
-  DEST_COPY="$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
-fi
+DEST_COPY="$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
 
 echo "Copying contents to git repo"
 if [ "$INPUT_DELETE_EXISTING" = "true" ]; then
