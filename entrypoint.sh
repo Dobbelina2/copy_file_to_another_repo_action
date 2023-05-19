5e2ce347e730542b9ab3bce13c873aa5718e882b
Dobbelina2 <132213227+Dobbelina2@users.noreply.github.com>
2023-05-19 10:39:25 +0200

    DEST_COPY change

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,7 +24,11 @@ git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
 git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@$INPUT_GIT_SERVER/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
-DEST_COPY="$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
+if [ -z "$INPUT_DESTINATION_FOLDER" ]; then
+  DEST_COPY="$CLONE_DIR"
+else
+  DEST_COPY="$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
+fi
 
 echo "Copying contents to git repo"
 if [ "$INPUT_DELETE_EXISTING" = "true" ]; then
