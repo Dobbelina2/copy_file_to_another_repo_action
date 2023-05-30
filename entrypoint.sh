914d03f286b5c3f0798d058978d1f390b44db9ef
Dobbelina2 <132213227+Dobbelina2@users.noreply.github.com>
2023-05-30 18:52:29 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -55,8 +55,11 @@ for SOURCE_FILE in $SOURCE_FILES; do
   # Trim leading/trailing whitespace
   SOURCE_FILE=$(echo "$SOURCE_FILE" | xargs)
   
-  # Handle source file
-rsync $INPUT_RSYNC_OPTION "$SOURCE_FILE" "$DEST_COPY"
+# Get the rsync options from action.yml
+RSYNC_OPTION=$INPUT_RSYNC_OPTION
+
+# Use eval to evaluate the rsync options as a command line argument
+eval "rsync $RSYNC_OPTION \"$SOURCE_FILE\" \"$DEST_COPY\""
 done
 
 cd "$CLONE_DIR"
